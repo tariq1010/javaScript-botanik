c46b06e675a2494407d222d4aa27f68f9bb5d1a4
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2023-01-05 15:53:46 +0500

    update

--- backend/App/helpers/index.ts
+++ backend/App/helpers/index.ts
@@ -1,6 +1,5 @@
 const Web3 = require("web3");
 const { CONTRACT_ADDRESS, CONTRACT_ABI } = require("../contract/index");
-const { environment } = require("../../environment");
 
 const rpcUrl = process.env.RPC_URL;
 const web3 = new Web3(rpcUrl);
